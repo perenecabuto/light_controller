906684ff417deed3174d4296c100f4473cf53490
Felipe Ramos <perenecabuto@gmail.com>
2013-03-26 02:23:39 -0300

    First commit

---
+++ Makefile
@@ -0,0 +1,9 @@
+ARDUINO_DIR = /usr/share/arduino
+ARDMK_DIR     = /usr
+AVR_TOOLS_DIR = /usr
+
+BOARD_TAG    = atmega328
+ARDUINO_PORT = /dev/ttyUSB0
+ARDUINO_LIBS = RCswitch
+
+include /usr/share/arduino/Arduino.mk
---
+++ build-atmega328/controlador_luz.cpp
@@ -0,0 +1,38 @@
+#include <Arduino.h>
+#include <RCSwitch.h>
+
+RCSwitch mySwitch = RCSwitch();
+
+void setup() {
+  Serial.begin(9600);
+  
+  mySwitch.enableTransmit(10);
+  mySwitch.setPulseLength(297);
+  //mySwitch.setProtocol(1);
+  //mySwitch.setRepeatTransmit(15);
+}
+
+void loop() {
+
+  while (Serial.available()) {
+    char msg = Serial.read();
+    if (msg == 'A') {
+      Serial.write("A!!!");
+      mySwitch.send(9221160, 24);      
+    }
+    else if (msg == 'B') {
+      Serial.write("B!!!");
+      mySwitch.send(9221156, 24);
+    }
+    else if (msg == 'C') {
+      Serial.write("C!!!");
+      mySwitch.send(9221153, 24);      
+    }
+    else if (msg == 'D') {
+      Serial.write("D!!!");
+      mySwitch.send(9221154, 24);
+    }
+  }
+ 
+
+}
